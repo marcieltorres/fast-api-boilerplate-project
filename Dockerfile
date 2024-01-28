3dba4c1ddef6ed680c67d43a8e8a580b2124c4cf
Marciel Torres <marcielribeirotorres@gmail.com>
2024-01-28 17:39:29 -0300

    Adding FastAPI

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.11.0-slim-buster AS base
 
+EXPOSE 8000
+
 WORKDIR /src
 
 COPY pyproject.toml .
@@ -16,3 +18,4 @@ FROM dependencies AS production
 COPY src src
 COPY settings.conf src
 COPY logging.conf src
+COPY run.py src
--- tests/unit/config/test_settings.py
+++ tests/unit/config/test_settings.py
@@ -6,7 +6,7 @@ from src.config.settings import Settings
 
 class SettingsTest(TestCase):
     def setUp(self) -> None:
-        self.settings = Settings(file='./tests/config/settings_to_test.conf')
+        self.settings = Settings(file='./tests/unit/config/settings_to_test.conf')
 
     def test_get_setting_value_with_success(self):
         self.assertEqual(self.settings.get('app_name'), 'app-name')
@@ -22,20 +22,20 @@ class SettingsTest(TestCase):
 
     @mock.patch.dict(os.environ, {'ENV': 'prod'}, clear=True)
     def test_get_setting_value_from_production_env_with_success(self):
-        prod_settings = Settings(file='./tests/config/settings_to_test.conf')
+        prod_settings = Settings(file='./tests/unit/config/settings_to_test.conf')
         self.assertEqual(prod_settings.get('app_var'), 'prod-app-var')
 
     @mock.patch.dict(os.environ, {'ENV': 'qa'}, clear=True)
     def test_get_setting_value_from_qa_env_with_success(self):
-        qa_settings = Settings(file='./tests/config/settings_to_test.conf')
+        qa_settings = Settings(file='./tests/unit/config/settings_to_test.conf')
         self.assertEqual(qa_settings.get('app_var'), 'qa-app-var')
 
     @mock.patch.dict(os.environ, {'ENV': 'test'}, clear=True)
     def test_get_setting_value_from_test_env_with_success(self):
-        test_settings = Settings(file='./tests/config/settings_to_test.conf')
+        test_settings = Settings(file='./tests/unit/config/settings_to_test.conf')
         self.assertEqual(test_settings.get('app_var'), 'test-app-var')
 
     @mock.patch.dict(os.environ, {'ENV': 'dev'}, clear=True)
     def test_get_setting_value_from_dev_env_with_success(self):
-        dev_settings = Settings(file='./tests/config/settings_to_test.conf')
+        dev_settings = Settings(file='./tests/unit/config/settings_to_test.conf')
         self.assertEqual(dev_settings.get('app_var'), 'dev-app-var')
